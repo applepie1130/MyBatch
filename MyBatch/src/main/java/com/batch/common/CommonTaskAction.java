6ff3281edf4959ffc6debfe13da32cec44740f5c
SungJun <SungJun@192.168.0.7>
2016-01-30 16:43:35 +0900

    [2016.01.30] Quartz를 통한 스케쥴링 방식으로 수정 (기존 서버에서 Crontab부분 삭제)

--- MyBatch/src/main/java/com/batch/common/CommonTaskAction.java
+++ MyBatch/src/main/java/com/batch/common/CommonTaskAction.java
@@ -2,28 +2,43 @@ package com.batch.common;
 
 import java.util.List;
 
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.scheduling.quartz.QuartzJobBean;
 
-public class CommonTaskAction {
+public class CommonTaskAction extends QuartzJobBean {
 	
-	public static <T> void run(List<Class> services, String... args) {
+	private static List<Class> services = null;
+	
+	public static <T> void run(List<Class> servicesPrm, String...args) {
+		// 스케쥴링을 위한 QaurtzContext
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("scheduleContext.xml");
+		context.registerShutdownHook();
 		
-		ClassPathXmlApplicationContext context = null;
+		services = servicesPrm;
+	}
+	
+	/**
+	 * @see	Qaurtz 실행함수 정의
+	 */
+	@Override
+	protected void executeInternal(JobExecutionContext arg0) throws JobExecutionException {
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		context.registerShutdownHook();
+		
 		ThreadPoolTaskExecutor taskExecutor = null;
 		
 		try {
-			context = new ClassPathXmlApplicationContext("applicationContext.xml");
-			context.registerShutdownHook();
-			
+			// // [S] ThreadPoolTaskExecutor Configuration
 			taskExecutor = (ThreadPoolTaskExecutor) context.getBean("taskExecutor");
-			
-			// ThreadPoolTaskExecutor Configuration
 			taskExecutor.setCorePoolSize(services.size());
 			taskExecutor.setMaxPoolSize(services.size() * 2);
 			taskExecutor.setQueueCapacity(taskExecutor.getCorePoolSize() * taskExecutor.getMaxPoolSize() * 2);
 			taskExecutor.setWaitForTasksToCompleteOnShutdown(true);
 			taskExecutor.initialize();
+			// [E] TestExecutor 설정
 			
 			// Thread Run!!
 			for ( int i=0; i<services.size(); i++ ) {
@@ -31,9 +46,7 @@ public class CommonTaskAction {
 				Runnable runner = (Runnable) context.getBean(service);
 				taskExecutor.execute(runner);
 			}
-			
 		} finally {
-			// All Close
 			taskExecutor.shutdown();
 			context.close();
 		}
